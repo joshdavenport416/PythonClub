7885e992f17d241d1ffaf65e84a5d6832ea4f5c1
Joshua <joshdavenport416@hotmail.com>
2019-04-25 12:26:17 -0700

    Assignment 5 Resource View

--- pythonclub/club/urls.py
+++ pythonclub/club/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('getresources/', views.getresources, name='resources'),
 ]
--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'club/index.html')
+
+def getresources(request):
+    resource_list=Resource.objects.all()
+    context={'resource_list' : resource_list }
+    return render(request, 'club/resources.html', context=context)
