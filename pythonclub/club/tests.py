f5c0066abc964be663b056bb21e5f23897b18274
Joshua <joshdavenport416@hotmail.com>
2019-05-28 12:06:53 -0700

    Assignment 10 More Tests

--- pythonclub/club/tests.py
+++ pythonclub/club/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from .models import Meeting, MeetingMinutes, Resource, Event
+import datetime
+from django.contrib.auth.models import User
+from .forms import ResourceForm
+
 
 # Create your tests here.
 #tests for models
@@ -34,3 +38,30 @@ class EventTest(TestCase):
 
     def test_table(self):
         self.assertEqual(str(Event._meta.db_table), 'club_event')
+
+# form tests
+class ResourceFormTest(TestCase):
+    def setUp(self):
+        self.user=User.objects.create(username='user1', password='P@ssw0rd1')
+
+    def test_ResourceForm(self):
+        data={
+            'resourcename' : 'resource1',
+            'resourcetype' : 'type1',
+            'dateentered' : datetime.date(2019,5,28),
+            'userid' : self.user,
+        }
+        form = ResourceForm(data=data)
+        self.assertTrue(form.is_valid)
+
+    def test_ResourceFormInvalid(self):
+        data = {
+            'resourcename': 'resource1',
+            'resourcetype': '',
+            'dateentered': datetime.date(2019, 5, 28),
+            'userid': self.user,
+        }
+        form = ResourceForm(data=data)
+        self.assertFalse(form.is_valid())
+
+
